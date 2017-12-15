7a51ef763209c6d29a16bf847ce920a3455a7524
lhp9916 <lhp9916@gmail.com>
2017-12-15 23:51:29 +0800

    finish basic page layout

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,2 +1,8 @@
 <?php
 //自定义辅助函数
+
+//将当前请求的路由名称转换为 CSS 类名称
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://weibo.com/lhp9916" target="_blank">刘海朋</a>编码 <span
+                    style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right">
+            <a href="mailto:lhp9916@gmail.com">联系我们</a>
+        </p>
+    </div>
+</footer>
