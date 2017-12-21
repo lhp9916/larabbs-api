b4a493bf5b52fe51decbbaf482c740efaae8dea3
lhp9916 <lhp9916@gmail.com>
2017-12-21 23:18:57 +0800

    category topic list

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,13 @@ Auth::routes();
 //用户
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
+//分类
+Route::resource('categories','CategoriesController',['only'=>'show']);
+
+//主题
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
 //测试
 Route::get('test', function (){
     phpinfo();
 });
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
