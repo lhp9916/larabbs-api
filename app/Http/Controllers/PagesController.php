3c744b68d40e083af56144869eec26e1bbf5337b
lhp9916 <lhp9916@gmail.com>
2017-12-28 16:42:14 +0800

    后台访问权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,15 @@ class PagesController extends Controller
     {
         return view('pages.root');
     }
+
+
+    public function permissionDenied()
+    {
+        //如果当前用户有权限登录后台，就直接跳转
+        if (config('administrator.permission')()) {
+            return redirect(url(config('administrator.uri')), 302);
+        }
+
+        return view('pages.permission_denied');
+    }
 }
