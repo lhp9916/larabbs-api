ea2091c3e82113f18444b1aea8ef32d321160c2f
刘海朋 <991695066@qq.com>
2018-11-14 10:18:01 +0800

    replies store

--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -2,20 +2,8 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
-
 class VerificationCodeRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -29,7 +17,7 @@ class VerificationCodeRequest extends FormRequest
                 'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/',
                 'unique:users',
             ],*/
-            'captcha_key' => 'required|string',
+            'captcha_key'  => 'required|string',
             'captcha_code' => 'required|string',
         ];
     }
@@ -37,7 +25,7 @@ class VerificationCodeRequest extends FormRequest
     public function attributes()
     {
         return [
-            'captcha_key' => '图片验证码 key',
+            'captcha_key'  => '图片验证码 key',
             'captcha_code' => '图片验证码',
         ];
     }
