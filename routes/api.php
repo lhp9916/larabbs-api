e95d882bd39b65ab0a4cb714046b6369709403d5
刘海朋 <991695066@qq.com>
2018-11-16 15:45:56 +0800

    users show

--- routes/api.php
+++ routes/api.php
@@ -56,6 +56,9 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+        // 用户详情
+        $api->get('users/{user}', 'UsersController@show')
+            ->name('api.users.show');
     });
 
     //每分钟60次
