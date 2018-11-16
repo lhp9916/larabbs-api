0b6db86d101855cea74f6f94c67e29649451f33f
刘海朋 <991695066@qq.com>
2018-11-16 10:45:40 +0800

    支持手机号登录

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,16 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function attemptLogin(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $username = $request->email;
+        if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+            $credentials['phone'] = $username;
+            unset($credentials['email']);
+        }
+        return $this->guard()->attempt($credentials, $request->filled('remember'));
     }
 }
